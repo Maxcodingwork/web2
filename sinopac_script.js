38d7d326c790af022493d8929fd2ee28ca20b44a
Maxcodingwork <104746670+Maxcodingwork@users.noreply.github.com>
2025-08-14 09:39:15 +0800

    Update sinopac_script.js

--- sinopac_script.js
+++ sinopac_script.js
@@ -32,6 +32,7 @@ window.collectEvent('start'); // 通知SDK设置完毕，可以真正开始发�
     backgroundSize: "cover",
     onClick: function(item, index) { 
       // 點擊回調，可自定義上報
+      console.log(item,index)
       if (item.navigate_url) {
       window.open(item.navigate_url, '_blank');
       }
